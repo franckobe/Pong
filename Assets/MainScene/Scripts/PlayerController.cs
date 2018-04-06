37d69fc67de71113da7c5563f81fde391c30f65e
franckobe <franck.garros@ynov.com>
2018-04-06 09:21:44 +0200

    first commit

---
+++ Assets/MainScene/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    float playerStep = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // transform.position = new Vector3(transform.position.x,0.0f, ray.GetPoint(8.0f).z);
+
+        rb.velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.MovePosition(new Vector3(transform.position.x, 0.0f, transform.position.z + playerStep));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(new Vector3(transform.position.x, 0.0f, transform.position.z - playerStep));
+        }
+
+    }
+}
---
+++ Assets/MainScene/Scripts/WallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    string wallName;
+    int direction;
+
+	// Use this for initialization
+	void Start () {
+        wallName = gameObject.name;
+        if (wallName == "wall_center_1") direction = -1;
+        if (wallName == "wall_center_2") direction = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (wallName == "wall_center_1")
+        {
+            if (transform.position.z <= 2.75 || transform.position.z >= 6) direction *= -1;
+        }
+        if (wallName == "wall_center_2")
+        {
+            if (transform.position.z >= -2.25 || transform.position.z <= -6) direction *= -1;
+        }
+        transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z + (0.10f * direction));
+
+    }
+}
